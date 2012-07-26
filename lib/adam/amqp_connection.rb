161b658cca07f51571cd2430483d8ecee8cd64d2
Weston Jossey <weston.jossey@gmail.com>
2012-07-26 15:28:28 -0400

    Change the options we can pass in for connecting to a server.

--- lib/adam/amqp_connection.rb
+++ lib/adam/amqp_connection.rb
@@ -5,11 +5,18 @@ module Adam
     SIZE = 1
 
     def self.create(options={})
-      url = options[:url] || '127.0.0.1'
+      connect_opts = {
+        :host => options[:url] || '127.0.0.1',
+        :port => options[:port] || 5672,
+        :user => options[:user] || 'guest',
+        :pass => options[:pass] || 'guest',
+        :vhost => options[:vhost] || '/',
+        :ssl => options[:ssl] || false,
+        :frame_max => options[:frame_max] || 131072
+      }
       # need a connection for Fetcher and Retry
-
       ConnectionPool.new(:timeout => 1, :size => SIZE) do
-        connection = EM::Synchrony::AMQP.connect(:host => url)
+        EM::Synchrony::AMQP.connect(connect_opts)
       end
     end
   end
